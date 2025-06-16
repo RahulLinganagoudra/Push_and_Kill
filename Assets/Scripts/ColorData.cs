b91bf47bd730d3b2895235e45132fcc4c673e478
Rahul Linganagoudra <slinganagoudra@gmail.com>
2025-06-16 17:00:20 +0530

    create color data

--- Assets/Scripts/ColorData.cs
+++ Assets/Scripts/ColorData.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Creative
@@ -15,6 +16,44 @@ namespace Creative
 	[CreateAssetMenu]
 	public class ColorData : ScriptableObject
 	{
+		static ColorData instance;
+		public static ColorData Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = Resources.Load<ColorData>("HexaBundleData");
+				}
+				return instance;
+			}
+		}
+		[System.Serializable]
+		public class VisualData
+		{
+			public ColorType colorType;
+			public Material material;
+		}
+		[SerializeField] List<VisualData> materials;
+		[SerializeField] float slotOffset = 0.5f;
 
+
+		public Vector3 GetStackedPosition(int index, Vector3 startPosition, float slotOffset)
+		{
+			Vector3 offset = index * slotOffset * Vector3.up;
+
+			return startPosition + offset; // Apply rotation to the offset
+		}
+		public Material GetMaterial(ColorType colorType)
+		{
+			foreach (var data in materials)
+			{
+				if (data.colorType == colorType)
+				{
+					return data.material;
+				}
+			}
+			return null; // or throw an exception if not found
+		}
 	}
 }
